176a4f1df8d109af06bf06856ed793a980df7511
Connor Forsyth <43639117+cFors4@users.noreply.github.com>
2020-02-16 18:30:45 +0000

    Update codeMessage.h

--- codeMessage.h
+++ codeMessage.h
@@ -1,4 +1,3 @@
-
 # include <stdio.h>
 # include <stdlib.h>
 # include <string.h>
@@ -15,9 +14,11 @@ struct Node *prev;
 
 /*function blueprints*/
 
+void error(char message[]);
 void getChars(char **s, char wheel_file[]);
 void createLinkedList(struct Node ** root, char wheelValues[]);
 void codeLetters(struct Node* root, char letters[], int **coded, int lenWheel);
 void decodeNumbers(struct Node* root, int numbers[], char **decoded, int len);
+
 
 
